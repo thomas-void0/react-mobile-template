730de6e72cce13a76d1453aaccf154994be41df8
newrankyangjingyu <yjy15680489038@163.com>
2022-01-14 17:45:51 +0800

    chore: 去除无用注释

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { useRoutes } from 'react-router-dom'
 import routes from './router'
 import '@/assets/styles/normalize.less'
 
-// 在非微信环境不能打开
 const App = (): ReactElement => {
 	const el = useRoutes(routes)
 	return <div>{el}</div>
