af16f67fb7ab8fd01a15eac1fb82b8c2c363e2b5
newrankyangjingyu <yjy15680489038@163.com>
2022-01-18 11:57:24 +0800

    ci: add react fresh

--- script/webpack.dev.conf.js
+++ script/webpack.dev.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const { merge } = require('webpack-merge')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const webpack = require('webpack')
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const proxyObj = {
 	'/xdnphb/common': {
@@ -33,19 +34,30 @@ module.exports = merge(base, {
 			//进行eslint检查
 			{
 				test: /\.(js|ts|tsx)$/,
-				loader: 'eslint-loader',
 				enforce: 'pre',
 				include: [path.join(__dirname, 'src')],
-				options: {
-					fix: true
-				},
-				exclude: /node_modules/
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							plugins: [require.resolve('react-refresh/babel')]
+						}
+					},
+					{
+						loader: 'eslint-loader',
+						options: {
+							fix: true
+						}
+					}
+				]
 			}
 		]
 	},
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
-		new ESLintPlugin({ extensions: ['js', 'jsx', 'ts', 'tsx'] })
+		new ESLintPlugin({ extensions: ['js', 'jsx', 'ts', 'tsx'] }),
+		new ReactRefreshPlugin() // 为 react-refresh 添加
 	],
 	devtool: 'source-map',
 	devServer: {
